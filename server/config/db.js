df9dd5e51a3a0eb29fad2928c80265420dfb1992
Shuraih <shuraih15@gmail.com>
2024-04-16 12:49:29 +0530

    update

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/bookmart_website');
+        await mongoose.connect('mongodb+srv://shuraih15:hz0V3zvzNcG3obMD@bookstore.bo7kawh.mongodb.net/');
         console.log('MongoDB Connected');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
